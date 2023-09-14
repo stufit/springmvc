6b1d1a648420e8933a4ae389831499167f0ee158
이관영 <71133204+stufit@users.noreply.github.com>
2023-09-14 17:33:16 +0900

    HTTP 요청 파라미터 - @ModelAttribute

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,13 +1,11 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Map;
@@ -95,6 +93,34 @@ public class RequestParamController {
     public String requestParamMap(@RequestParam Map<String, Object> paramMap
                                   ) {
         log.info("username = {}, age = {}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    @ResponseBody
+    @GetMapping("/model-attribute-v1")
+    public String modelAttributeV1(@RequestParam String username,
+                                   @RequestParam int age){
+        HelloData helloData = new HelloData();
+        helloData.setUsername(username);
+        helloData.setAge(age);
+        log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData = {}", helloData);
+        return "ok";
+    }
+
+    @ResponseBody
+    @GetMapping("/model-attribute-v2")
+    public String modelAttributeV2(@ModelAttribute HelloData helloData){
+        log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData = {}", helloData);
+        return "ok";
+    }
+
+    @ResponseBody
+    @GetMapping("/model-attribute-v3")
+    public String modelAttributeV3(HelloData helloData){
+        log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData = {}", helloData);
         return "ok";
     }
 
